34176c69b8817ace35de60569fa2fec6bb9f2bc5
Guilherme Bernal <dev@lbguilherme.com>
2015-04-26 18:56:36 -0300

    Allow usage of facebook.me on QML

--- facebook.hpp
+++ facebook.hpp
@@ -12,7 +12,7 @@ class QUrlQuery;
 class Facebook : public QObject{
     Q_OBJECT
     Q_PROPERTY(QString accessToken READ accessToken WRITE setAccessToken NOTIFY accessTokenChanged)
-    Q_PROPERTY(FacebookUser* me READ me)
+    Q_PROPERTY(FacebookUser* me READ me NOTIFY meChanged)
 public:
 
     Facebook();
@@ -26,6 +26,7 @@ public:
 
 signals:
 
+    void meChanged();
     void accessTokenChanged();
     void javaCallbackLoginSuccess();
     void javaCallbackLoginError();
