951901b410efbbd77618a2a383f6083cb159b25d
Guilherme Bernal <dev@lbguilherme.com>
2015-04-26 18:45:42 -0300

    Log AppEvents for facebook statistics

--- android/src/vc/duvido/DuvidoActivity.java
+++ android/src/vc/duvido/DuvidoActivity.java
@@ -1,12 +1,13 @@
 package vc.duvido;
 
-import org.qtproject.qt5.android.bindings.QtActivity;
+import org.qtproject.qt5.android.bindings.*;
 import java.lang.*;
 import java.util.*;
 import android.app.*;
 import android.content.*;
 import android.util.*;
 import android.os.*;
+import com.facebook.appevents.*;
 
 public class DuvidoActivity extends QtActivity {
 
@@ -23,6 +24,18 @@ public class DuvidoActivity extends QtActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         FacebookBridge.submitActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        AppEventsLogger.deactivateApp(this);
     }
 
 }
