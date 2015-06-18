bd1a803fd3edfeeaa9e815431e52504e99081327
Guilherme Bernal <dev@lbguilherme.com>
2015-06-18 13:55:56 -0300

    Remove legacy api code

--- duvidoapi.cpp
+++ duvidoapi.cpp
@@ -13,27 +13,3 @@ QUrl DuvidoApi::avatarUrl(QString id) {
 QUrl DuvidoApi::avatarsUrl(QStringList ids) {
     return apiUrl + "/avatars?id=" + ids.join(",");
 }
-
-/*
-void DuvidoApi::upload(QString token, QByteArray data, std::function<void(QString)> callback, std::function<void(double)> uploadProgress) {
-    apiCall("post", "/upload", QVariantMap{{"token", token}}, data, [=](QByteArray bytes){
-        callback(QString::fromUtf8(bytes));
-    }, uploadProgress);
-}
-
-void DuvidoApi::createChallenge(QString title, QString description, QString reward, QStringList targets,
-                                unsigned duration, QString imageId, std::function<void()> callback) {
-    QVariantMap args {
-        {"title", title},
-        {"description", description},
-        {"reward", reward},
-        {"targets", targets.join(",")},
-        {"duration", QString::number(duration)},
-        {"image", imageId}
-    };
-
-    apiCall("post", "/challenge", args, {}, [=](QByteArray){
-        callback();
-    });
-}
-*/
