7e6524cdbbc14f976f60c85909d684d7d76af67f
Guilherme Bernal <dev@lbguilherme.com>
2015-07-25 02:18:38 -0300

    Remove extra fields on Me that are no longer sent by the server

--- data/me.cpp
+++ data/me.cpp
@@ -2,26 +2,14 @@
 
 QJsonObject Me::toJson() const {
     QJsonObject obj = User::toJson();
-    obj["firstName"] = firstName;
-    obj["lastName"] = lastName;
-    obj["birthday"] = birthday.toString("MM/dd/yyyy");
-    obj["gender"] = gender;
-    obj["email"] = email;
     return obj;
 }
 
 Me& Me::fromJson(const QJsonObject& obj) {
     User::fromJson(obj);
-    firstName = obj["firstName"].toString();
-    lastName = obj["lastName"].toString();
-    birthday = QDate::fromString(obj["birthday"].toString(), "MM/dd/yyyy");
-    gender = obj["gender"].toString();
-    email = obj["email"].toString();
     return *this;
 }
 
 bool Me::operator==(const Me& other) {
-    return User::operator==(other) &&
-            firstName == other.firstName && lastName == other.lastName &&
-            birthday == other.birthday && gender == other.gender && email == other.email;
+    return User::operator==(other);
 }
--- data/me.hpp
+++ data/me.hpp
@@ -7,10 +7,4 @@ struct Me : User {
     QJsonObject toJson() const;
     Me& fromJson(const QJsonObject& obj);
     bool operator==(const Me& other);
-
-    QString firstName;
-    QString lastName;
-    QDate birthday;
-    QString gender;
-    QString email;
 };
