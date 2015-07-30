5292748a47f35d0a1b96e65696f69cc2d16eb1b6
Guilherme Bernal <dev@lbguilherme.com>
2015-07-30 19:23:49 -0300

    Implement launching notifications from both Mixpanel and our server

--- android/src/vc/duvido/gcm/MessageListenerService.java
+++ android/src/vc/duvido/gcm/MessageListenerService.java
@@ -1,12 +1,62 @@
 package vc.duvido.gcm;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import com.google.android.gms.gcm.GcmListenerService;
 
+import java.util.Random;
+
+import vc.duvido.DuvidoActivity;
+import vc.duvido.R;
+
 public class MessageListenerService extends GcmListenerService {
     @Override
     public void onMessageReceived(String from, Bundle data) {
+        Intent intent = new Intent(this, DuvidoActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
+        String title = "";
+        String body = "";
+
+        if (data.containsKey("mp_message")) { // Mixpanel's notification
+            title = "Duvido.vc";
+            body = data.getString("mp_message");
+        } else if (data.containsKey("type")) { // Standard Duvido.vc server message
+            String type = data.getString("type");
+            if (type.equals("basic-forward")) {
+                title = data.getString("title");
+                body = data.getString("body");
+            } else {
+                // This type is unknown. Likely meant for a more up-to-date app.
+                return;
+            }
+        } else {
+            // What message is this?
+            return;
+        }
+
+        Bitmap duvidoIcon = BitmapFactory.decodeResource(getResources(), R.drawable.icon);
+        duvidoIcon = Bitmap.createScaledBitmap(duvidoIcon, 64, 64, false);
+
+        Notification notification = new Notification.Builder(this)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(R.drawable.notification_white)
+                .setLargeIcon(duvidoIcon)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true)
+                .setVibrate(new long[]{0, 200, 100, 400})
+                .getNotification();
+
+        NotificationManager notificationManager =
+                (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+        notificationManager.notify(new Random().nextInt(), notification);
     }
 }
