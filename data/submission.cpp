03026b16781ad92fa92f4173017e874c21a04a63
Guilherme Bernal <dev@lbguilherme.com>
2015-09-07 22:55:23 -0300

    Add extra fields on submission

--- data/submission.cpp
+++ data/submission.cpp
@@ -2,21 +2,44 @@
 
 QJsonObject Submission::toJson() const {
     QJsonObject obj = PreSubmission::toJson();
+    obj["id"] = id;
     obj["status"] = status;
     obj["sentTime"] = QString::number(sentTime.toMSecsSinceEpoch());
-    obj["judgedTime"] = QString::number(judgedTime.toMSecsSinceEpoch());
+    obj["judgedTime"] = judgedTime.isNull() ? "" : QString::number(judgedTime.toMSecsSinceEpoch());
+    obj["creationTime"] = QString::number(creationTime.toMSecsSinceEpoch());
+    obj["challengeOwner"] = challengeOwner.toJson();
+    obj["title"] = title;
+    obj["details"] = details;
+    obj["reward"] = reward;
+    obj["duration"] = (double)duration;
+    obj["ratio"] = ratio;
     return obj;
 }
 
 Submission& Submission::fromJson(const QJsonObject& obj) {
     PreSubmission::fromJson(obj);
+    id = obj["id"].toString();
     status = obj["status"].toString();
     sentTime = QDateTime::fromMSecsSinceEpoch(obj["sentTime"].toString().toLongLong());
-    judgedTime = QDateTime::fromMSecsSinceEpoch(obj["judgedTime"].toString().toLongLong());
+    judgedTime = obj["judgedTime"].toString() == "" ? QDateTime() : QDateTime::fromMSecsSinceEpoch(obj["judgedTime"].toString().toLongLong());
+    title = obj["title"].toString();
+    details = obj["details"].toString();
+    reward = obj["reward"].toString();
+    duration = obj["duration"].toDouble();
+    creationTime = QDateTime::fromMSecsSinceEpoch(obj["creationTime"].toString().toLongLong());
+    challengeOwner.fromJson(obj["challengeOwner"].toObject());
+    ratio = obj["ratio"].toDouble();
     return *this;
 }
 
 bool Submission::operator==(const Submission& other) {
-    return PreSubmission::operator==(other) && status == other.status &&
-            sentTime == other.sentTime && judgedTime == other.judgedTime;
+    return PreSubmission::operator==(other) && id == other.id && title == other.title &&
+            details == other.details && reward == other.reward && duration == other.duration &&
+            status == other.status && sentTime == other.sentTime && judgedTime == other.judgedTime &&
+            creationTime == other.creationTime && challengeOwner == other.challengeOwner &&
+            ratio == other.ratio;
+}
+
+int Submission::timeLeft() const {
+    return duration - (QDateTime::currentMSecsSinceEpoch() - creationTime.toMSecsSinceEpoch())/1000;
 }
--- data/submission.hpp
+++ data/submission.hpp
@@ -1,22 +1,41 @@
 #pragma once
 
 #include <data/presubmission.hpp>
+#include <data/user.hpp>
 #include <QJsonObject>
 #include <QDateTime>
 #include <QObject>
 
 struct Submission : PreSubmission {
     Q_GADGET
+    Q_PROPERTY(QString id MEMBER id CONSTANT)
     Q_PROPERTY(QString status MEMBER status CONSTANT)
     Q_PROPERTY(QDateTime sentTime MEMBER sentTime CONSTANT)
     Q_PROPERTY(QDateTime judgedTime MEMBER judgedTime CONSTANT)
+    Q_PROPERTY(User challengeOwner MEMBER challengeOwner CONSTANT)
+    Q_PROPERTY(QDateTime creationTime MEMBER creationTime CONSTANT)
+    Q_PROPERTY(QString title MEMBER title CONSTANT)
+    Q_PROPERTY(QString details MEMBER details CONSTANT)
+    Q_PROPERTY(QString reward MEMBER reward CONSTANT)
+    Q_PROPERTY(unsigned duration MEMBER duration CONSTANT)
+    Q_PROPERTY(double ratio MEMBER ratio CONSTANT)
 public:
 
     QJsonObject toJson() const;
     Submission& fromJson(const QJsonObject& obj);
     bool operator==(const Submission& other);
 
+    int timeLeft() const;
+
+    QString id;
     QString status;
+    User challengeOwner;
+    QDateTime creationTime;
     QDateTime sentTime;
     QDateTime judgedTime;
+    QString title;
+    QString details;
+    QString reward;
+    unsigned duration = 0;
+    double ratio = 0;
 };
